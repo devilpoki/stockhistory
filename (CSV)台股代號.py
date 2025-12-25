2f65c0503e5806a51e641231d06e8c739d7cc06a
devilpoki <41864495+devilpoki@users.noreply.github.com>
2025-12-25 16:41:11 +0800

    Add files via upload

--- (CSV)台股代號.py
+++ (CSV)台股代號.py
@@ -1,73 +1,71 @@
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
-import random
-import time
 import csv
 
-# 上市
-urlmark1 = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y"
+# 定義抓取資料的函式
+def fetch_data(url):
+    # 送出GET請求
+    response = requests.get(url)
+    response.encoding = 'big5'  # 設置編碼
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# 上櫃
-urlmark2 = 'https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=2&issuetype=4&industry_code=&Page=1&chklike=Y'
+    # 打印HTML，檢查表格結構
+    # print(soup.prettify())  # 可以檢查表格的結構
 
-headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
+    # 找到股票資料表格
+    table = soup.find('table', class_='h4')
 
-res1 = requests.get(urlmark1 , headers = headers)
-res2 = requests.get(urlmark2 , headers = headers)
-#print(res.text)
+    # 檢查是否成功找到表格
+    if table:
+        print("Table found!")
+    else:
+        print("Table not found!")
 
-df1 = pd.read_html(res1.text)[0]
-#print(df1)
-df1= df1.drop([0,1,5,8,9],axis = 1)
-df1.columns = df1.iloc[0]
-df1 = df1.iloc[1:]
+    # 取得表格的標題
+    headers = [header.get_text(strip=True) for header in table.find_all('th')]
 
-# df = df.set_index("有價證券代號")
+    # 如果標題為空，則可能要手動處理
+    if not headers:
+        print("No headers found, trying another way to extract columns...")
+        # headers = ['Column{}'.format(i) for i in range(1, 11)]  # 假設最多10列
+        headers = ["頁面編號","國際證券編碼","有價證券代號","有價證券名稱","市場別","有價證券別","產業別","公開發行/上市(櫃)/發行日","CFICode","備註"]
 
-######
-df2 = pd.read_html(res2.text)[0]
-df2= df2.drop([0,1,5,8,9],axis = 1)
-df2.columns = df2.iloc[0]
-df2= df2.iloc[1:]
-# print(df2)
+    # 取得表格的每一行資料
+    rows = []
+    for row in table.find_all('tr')[1:]:  # 跳過表頭
+        cols = row.find_all('td')
+        if len(cols) > 1:
+            row_data = [col.get_text(strip=True) for col in cols]
+            rows.append(row_data)
 
-# 兩個datafram 合併
-frames = [df1, df2]
-df = pd.concat(frames)
-# print(df)
+    # 返回DataFrame
+    return pd.DataFrame(rows, columns=headers)
 
-df.to_csv('D:/TWSE 台股/台股股票代號.csv', index=False, encoding= 'big5hkscs')
+# 網址1
+url1 = 'https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y'
 
-######  Goodinfo 現金流量網址 +  股號
+# 網址2
+url2 = 'https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=2&issuetype=4&industry_code=&Page=1&chklike=Y'
 
-stock_num = df['有價證券代號']
-urlbase  = 'https://goodinfo.tw/tw/StockCashFlow.asp?STOCK_ID='
-urls = [urlbase + str(stock) for stock in stock_num]
+# 抓取資料
+df1 = fetch_data(url1)
+df2 = fetch_data(url2)
 
+# 刪除第一,二欄（頁面編號,國際證券編碼)
+df1 = df1.iloc[:, 2:]
+df2 = df2.iloc[:, 2:]
 
-# 生成一個隨機的秒數，範圍可以自行調整
-# random_seconds = random.randint(3, 9)  # 生成0到59之間的隨機整數
-# time.sleep(random_seconds)
-# print(urls)
+# 🔑 若欄位超過 8，保留前 8 欄
+df1 = df1.iloc[:, :7]
+df2 = df2.iloc[:, :7]
 
+# 合併兩個DataFrame
+df_combined = pd.concat([df1, df2], ignore_index=True)
 
-# 將Goodinfo 網址加上 股票代號 urls 結果寫成csv
-csv_file_name = 'D:/TWSE 台股/Goodinfo網址加股票代號.csv'
-with open(csv_file_name, 'w', newline='') as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerows([[url] for url in urls])
-    
+# 顯示合併後的DataFrame
+print(df_combined)
 
+df_combined.to_csv('D:\\TWSE 台股\\台股股票代號.csv', index=False, encoding='utf-8-sig')
+df_combined.to_csv('D:\\TWSE 台股\\台股股票代號.txt', sep=',', index=False, encoding='utf-8-sig')
 
-#===========================================  csv 讀出股號
-# import csv
-# from itertools import islice
-
-# fn = 'd:/TWSE 台股/台股股票代號.csv'
-
-# with open(fn) as csvfile:
-#     csvReader = csv.reader(csvfile)
-#     listReader = list(csvReader)
-    
-# for row in islice(listReader, 1, None):
-#     print(row[0])
